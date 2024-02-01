3c05df9aaf748e1c70b295425a3a4f41fd8ed33e
benjy <benjyfri96@gmail.com>
2024-02-01 12:55:27 +0200

    Fixed data such that it will contain only surfaces, saddles, bowls, ridges

--- build_surfaces.py
+++ build_surfaces.py
@@ -42,86 +42,48 @@ def createDataSet():
     new_file_path_test = "test_surfaces.h5"
     with h5py.File(new_file_path_train, "w") as new_hdf5_train_file:
         point_clouds_group = new_hdf5_train_file.create_group("point_clouds")
-        counter = 0
-        for gau_cur in [0, 1, -1]:
-            for mean_cur in [0, 1, -1]:
-                if gau_cur == 1 and mean_cur == 0:
-                    continue
-                # saddles are all the same!
-                if gau_cur == -1:
-                    for k in range(5000):
-                        a, b, c, d, e, _, H, K = createFunction(gaussian_curv=-1, mean_curv=-33)
-                        point_cloud = samplePoints(a, b, c, d, e, count=20)
-                        point_clouds_group.create_dataset(f"point_cloud_{counter}", data=point_cloud)
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['a'] = a
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['b'] = b
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['c'] = c
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['d'] = d
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['e'] = e
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['H'] = H
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['K'] = K
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['class'] = counter // 5000
-                        counter += 1
-                        if counter % 500 == 0:
-                            print(f'Counter is = {counter}')
-                    break
-                for k in range(5000):
-                    a, b, c, d, e, _, H, K = createFunction(gaussian_curv=gau_cur, mean_curv=mean_cur)
-                    point_cloud = samplePoints(a, b, c, d, e, count=20)
-                    point_clouds_group.create_dataset(f"point_cloud_{counter}", data=point_cloud)
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['a'] = a
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['b'] = b
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['c'] = c
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['d'] = d
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['e'] = e
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['H'] = H
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['K'] = K
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['class'] = counter // 5000
-                    counter += 1
-                    if counter % 500 == 0:
-                        print(f'Counter is = {counter}')
+        addDataToSet(point_clouds_group, gaussian_curv=0, mean_curv=0, label=0, counter=0, amount_of_pcl=10000,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=1, mean_curv=1, label=1, counter=10000, amount_of_pcl=5000,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=1, mean_curv=-1, label=1, counter=15000, amount_of_pcl=5000,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=0, mean_curv=1, label=2, counter=20000, amount_of_pcl=5000,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=0, mean_curv=-1, label=2, counter=25000, amount_of_pcl=5000,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=-1, mean_curv=-33, label=3, counter=30000, amount_of_pcl=10000,
+                     size_of_pcl=20)
 
     with h5py.File(new_file_path_test, "w") as new_hdf5_test_file:
         point_clouds_group = new_hdf5_test_file.create_group("point_clouds")
-        counter = 0
-        for gau_cur in [0, 1, -1]:
-            for mean_cur in [0, 1, -1]:
-                if gau_cur == 1 and mean_cur == 0:
-                    continue
-                # saddles are all the same!
-                if gau_cur == -1:
-                    for k in range(500):
-                        a, b, c, d, e, _, H, K = createFunction(gaussian_curv=-1, mean_curv=-33)
-                        point_cloud = samplePoints(a, b, c, d, e, count=20)
-                        point_clouds_group.create_dataset(f"point_cloud_{counter}", data=point_cloud)
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['a'] = a
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['b'] = b
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['c'] = c
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['d'] = d
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['e'] = e
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['H'] = H
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['K'] = K
-                        point_clouds_group[f"point_cloud_{counter}"].attrs['class'] = counter // 500
-                        counter += 1
-                        if counter % 100 == 0:
-                            print(f'Counter is = {counter}')
-                            plotFunc(a, b, c, d, e)
-                    break
-                for k in range(500):
-                    a, b, c, d, e, _, H, K = createFunction(gaussian_curv=gau_cur, mean_curv=mean_cur)
-                    point_cloud = samplePoints(a, b, c, d, e, count=20)
-                    point_clouds_group.create_dataset(f"point_cloud_{counter}", data=point_cloud)
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['a'] = a
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['b'] = b
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['c'] = c
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['d'] = d
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['e'] = e
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['H'] = H
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['K'] = K
-                    point_clouds_group[f"point_cloud_{counter}"].attrs['class'] = counter // 500
-                    counter += 1
-                    if counter % 500 == 0:
-                        print(f'Counter is = {counter}')
+        addDataToSet(point_clouds_group, gaussian_curv=0, mean_curv=0, label=0, counter=0, amount_of_pcl=1000,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=1, mean_curv=1, label=1, counter=1000, amount_of_pcl=500,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=1, mean_curv=-1, label=1, counter=1500, amount_of_pcl=500,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=0, mean_curv=1, label=2, counter=2000, amount_of_pcl=500,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=0, mean_curv=-1, label=2, counter=2500, amount_of_pcl=500,
+                     size_of_pcl=20)
+        addDataToSet(point_clouds_group, gaussian_curv=-1, mean_curv=-33, label=3, counter=3000, amount_of_pcl=1000,
+                     size_of_pcl=20)
+
+def addDataToSet(point_clouds_group, gaussian_curv, mean_curv, label, counter, amount_of_pcl, size_of_pcl=20):
+    for k in range(500):
+        a, b, c, d, e, _, H, K = createFunction(gaussian_curv=gaussian_curv, mean_curv=mean_curv)
+        point_cloud = samplePoints(a, b, c, d, e, count=20)
+        point_clouds_group.create_dataset(f"point_cloud_{counter+k}", data=point_cloud)
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['a'] = a
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['b'] = b
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['c'] = c
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['d'] = d
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['e'] = e
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['H'] = H
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['K'] = K
+        point_clouds_group[f"point_cloud_{counter+k}"].attrs['class'] = label
+    plotFunc(a, b, c, d, e, point_cloud)
 def createFunction(gaussian_curv, mean_curv, epsilon=0.05):
     if gaussian_curv==1 and mean_curv==0:
         raise ValueError("gaussian_curv==1 and mean_curv==0 is impossible")
@@ -167,8 +129,43 @@ def createFunction(gaussian_curv, mean_curv, epsilon=0.05):
 
     return a, b, c, d, e, count , H , K
 
+import plotly.graph_objects as go
 
-def plotFunc(a, b, c, d, e):
+def plotPcl(a, b, c, d, e, sample_count=20):
+    # Generate 20 random points within the specified range
+    sampled_points = samplePoints(a, b, c, d, e, count=sample_count)
+
+    # Create 3D scatter plot for sampled points using Plotly Express
+    fig = go.Figure()
+
+    # Add sampled points to the figure
+    fig.add_trace(go.Scatter3d(x=[sampled_points[0][0]],
+                               y=[sampled_points[0][1]],
+                               z=[sampled_points[0][2]],
+                               mode='markers',
+                               marker=dict(size=8, color='blue'),
+                               text=['1'],
+                               textposition='middle center',
+                               name='First Point'))
+
+    fig.add_trace(go.Scatter3d(x=[point[0] for point in sampled_points[1:]],
+                               y=[point[1] for point in sampled_points[1:]],
+                               z=[point[2] for point in sampled_points[1:]],
+                               mode='markers',
+                               marker=dict(size=8, color='red'),
+                               text=[f'{i + 2}' for i in range(sample_count - 1)],
+                               textposition='middle center',
+                               name='Rest of Points'))
+
+    # Customize layout
+    fig.update_layout(scene=dict(xaxis_title='X', yaxis_title='Y', zaxis_title='Z'),
+                      title="Sampled Points")
+
+    # Show the plot
+    fig.show()
+
+
+def plotFunc(a, b, c, d, e,sampled_points):
     # Create a grid of points for the surface
     x = np.linspace(-1, 1, 100)
     y = np.linspace(-1, 1, 100)
@@ -188,8 +185,6 @@ def plotFunc(a, b, c, d, e):
                         color_continuous_scale=['blue', 'red'], title="Generated Surface",
                         labels={'x': 'X', 'y': 'Y', 'z': 'Z'}, range_color=[0, 1])
 
-    # Plot the 21 sampled points on the surface
-    sampled_points = samplePoints(a, b, c, d, e, count=20)
     for i, point in enumerate(sampled_points):
         fig.add_trace(go.Scatter3d(x=[point[0]], y=[point[1]], z=[point[2]],
                                    mode='markers+text', text=[f'{i + 1}'],
@@ -197,17 +192,66 @@ def plotFunc(a, b, c, d, e):
 
     # Show the plot
     fig.show()
+def plotMultiplePcls(parameter_sets, index=1):
+    # Create a Plotly figure
+    fig = go.Figure()
+    import matplotlib.cm as cm
+
+    # Number of colors needed
+    num_colors = len(parameter_sets)  # You can adjust this based on the number of point clouds
+
+    # Create an array of colors using the 'viridis' colormap
+    # colors = cm.Paired(np.linspace(0, 1, num_colors))
+    colors = ['red', 'blue', 'green', 'orange', 'purple', 'yellow']
+    names = ['surface', 'valley', 'ridge', 'bowl', 'mountain', 'saddle']
+    # Iterate over each set of parameters
+    for i, params in enumerate(parameter_sets):
+        if i != index and i>0:
+            continue
+        # Generate sampled points for the current set of parameters
+        sampled_points = samplePoints(*params, count=20)
+
+        color = colors[i]
+        name = names[i]
+        fig.add_trace(go.Scatter3d(x=[point[0] for point in sampled_points],
+                                   y=[point[1] for point in sampled_points],
+                                   z=[point[2] for point in sampled_points],
+                                   mode='markers',
+                                   marker=dict(size=8, color=color),
+                                   text=[f'{j + 1}' for j in range(20)],
+                                   textposition='middle center',
+                                   name=f'{name}'))
+
+    fig.add_trace(go.Scatter3d(x=[sampled_points[0][0]],
+                               y=[sampled_points[0][1]],
+                               z=[sampled_points[0][2]],
+                               mode='markers',
+                               marker=dict(size=8, color='yellow'),
+                               text=['1'],
+                               textposition='middle center',
+                               name='First Point'))
+    # Customize layout
+    fig.update_layout(scene=dict(xaxis_title='X', yaxis_title='Y', zaxis_title='Z'),
+                      title="Sampled Point Clouds in Different Colors",
+                      showlegend=True)
+
+    # Show the plot
+    fig.show()
 if __name__ == '__main__':
     createDataSet()
-    # for i in [0,1,-1]:
-    #     for j in [0,1,-1]:
-    #         if i==1 and j==0:
-    #             continue
-    #         sum = 0
-    #         for k in range(1000):
-    #             a, b, c, d, e , count, H , K  = createFunction(gaussian_curv=i, mean_curv=j)
-    #             sum += count
-    #         print(f'Avg runs for gaussian_curv={i}, mean_curv={j} = {sum/1000} ')
-    #         plotFunc(a,b,c,d,e)
-
+    # parameter_sets = [
+    #     (-0.317246927541051, -1.4981434283801311, -0.8403160811352457, 0.45210163040038864, 4.989508427603763),
+    #
+    #     (3.6184079799238305, 0.7546575719059714, 3.3933402363050025, 1.4572342266597076, -4.891390159508835),
+    #
+    #     (-0.25023974028386053, -4.153200676255113, 2.0461446225436664, -4.655450773639295, 0.7272193519327672),
+    #
+    #     (3.838197172376347, 4.353654426721661, 2.9045692276212574, -3.943710916344255, 1.1583435215106501),
+    #
+    #     (-4.056479577616709, -2.2233117868694774, -3.0415162535894478, -3.2630328492056746, -2.682355605186225),
+    #
+    #     (1.2879827469937988, 2.192264473460156, -4.331727514839182, 0.06295771570294662, -0.010133464803817382)
+    # ]
+    # for i in range(1,6):
+    #     plotMultiplePcls(parameter_sets, i)
     print("yay")
