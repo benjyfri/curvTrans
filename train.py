665c56402295aca1ee13300721f462e3e55cf27a
benjy <benjyfri96@gmail.com>
2024-05-01 14:03:11 +0300

    Cleanup + contrastive weight option

--- train.py
+++ train.py
@@ -9,6 +9,9 @@ import torch
 import torch.nn as nn
 from models import MLP, TransformerNetwork, TransformerNetworkPCT, shapeClassifier
 from data import BasicPointCloudDataset
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def test(model, dataloader, loss_function, device, args):
@@ -79,6 +82,7 @@ def train_and_test(args):
     tripletMarginLoss = nn.TripletMarginLoss(margin=2.0)
     criterion = nn.CrossEntropyLoss(reduction='mean')
     mseLoss = nn.MSELoss()
+    contr_loss_weight = args.contr_loss_weight
     # Training loop
     for epoch in range(num_epochs):
         model.train()  # Set the model to training mode
@@ -118,7 +122,7 @@ def train_and_test(args):
                         total_train_contrastive_positive_loss += mseLoss(orig_emb, pos_emb)
                         total_train_contrastive_negative_loss += mseLoss(orig_emb, neg_emb)
                     total_train_contrastive_loss += loss2
-
+                    loss2 = (contr_loss_weight) * loss2
                 else:
                     output  = model((pcl.permute(0, 2, 1)).unsqueeze(2))
                     loss = criterion(output, label)
@@ -174,20 +178,16 @@ def configArgsPCT():
                         help='number of episode to train ')
     parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                         help='learning rate (default: 0.001, 0.1 if using sgd)')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
     parser.add_argument('--use_wandb', type=int, default=0, metavar='N',
                         help='use angles in learning ')
     parser.add_argument('--use_second_deg', type=int, default=0, metavar='N',
                         help='use second degree embedding ')
     parser.add_argument('--lpe_normalize', type=int, default=0, metavar='N',
                         help='use PCT transformer version')
-    parser.add_argument('--use_pct', type=int, default=0, metavar='N',
-                        help='use PCT transformer version')
     parser.add_argument('--std_dev', type=float, default=0, metavar='N',
                         help='amount of noise to add to data')
-    parser.add_argument('--use_mlp', type=int, default=0, metavar='N',
-                        help='use MLP version')
+    parser.add_argument('--contr_loss_weight', type=float, default=1.0, metavar='N',
+                        help='weight of contrastive loss')
     parser.add_argument('--lpe_dim', type=int, default=3, metavar='N',
                         help='laplacian positional encoding amount of eigens to take')
     parser.add_argument('--use_xyz', type=int, default=1, metavar='N',
@@ -198,24 +198,16 @@ def configArgsPCT():
                         help='use contrastive loss with middle layer (one before last)')
     parser.add_argument('--rotate_data', type=int, default=0, metavar='N',
                         help='use rotated data')
-    parser.add_argument('--num_of_heads', type=int, default=1, metavar='N',
-                        help='how many attention heads to use')
     parser.add_argument('--num_neurons_per_layer', type=int, default=64, metavar='N',
                         help='how many neurons per layer to use')
     parser.add_argument('--num_mlp_layers', type=int, default=4, metavar='N',
                         help='how many mlp layers to use')
-    parser.add_argument('--num_of_attention_layers', type=int, default=1, metavar='N',
-                        help='how many attention layers to use')
-    parser.add_argument('--att_per_layer', type=int, default=4, metavar='N',
-                        help='how many attention heads in each layer')
     parser.add_argument('--output_dim', type=int, default=4, metavar='N',
                         help='how many labels are used')
     parser.add_argument('--lr_jumps', type=int, default=50, metavar='N',
                         help='Lower lr *0.1 every amount of jumps')
     parser.add_argument('--sampled_points', type=int, default=40, metavar='N',
                         help='How many points where sampled around centroid')
-    parser.add_argument('--PE_dim', type=int, default=0, metavar='N',
-                        help='Positional embedding size')
     args = parser.parse_args()
     return args
 def testPretrainedModel(args, model=None):
@@ -294,70 +286,7 @@ def testPretrainedModel(args, model=None):
             print(f"  - median K for wrong predictions: {np.median(wrong_K_values[label])}")
 
     # return test_acc, label_accuracies, wrong_predictions_stats
-from torch.autograd import Variable
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-def input_visualized_importance(model_name='MLP3layers64Nlpe6xyzRotStd005'):
-    input_data = torch.randn(1, 3, 1, 41)  # Assuming input_size is the size of input to the model
-    # input_data = input_data*0 + 5*torch.randn(1, 3, 1, 41)  # Assuming input_size is the size of input to the model
-    # input_data = torch.ones(1, 3, 1, 41)  # Assuming input_size is the size of input to the model
-    # input_data_loaded = torch.load('pcl.pt').to('cpu')
-    # input_data_loaded.requires_grad = True
-    # input_data_loaded = input_data_loaded.float()
-    # input_data = input_data_loaded.clone() # Assuming input_size is the size of input to the model
-    input_data[0, 0, :, 0] = 0
-    input_data[0, 1, :, 0] = 0
-    input_data[0, 2, :, 0] = 0
-
-    args_shape = configArgsPCT()
-    args_shape.batch_size = 1024
-    args_shape.exp = model_name
-    args_shape.use_mlp = 1
-    args_shape.lpe_dim = 6
-    args_shape.num_mlp_layers = 3
-    args_shape.num_neurons_per_layer = 64
-    args_shape.sampled_points = 40
-    args_shape.use_second_deg = 0
-    args_shape.lpe_normalize = 1
-    model = shapeClassifier(args_shape)
-    model.load_state_dict(torch.load(f'{model_name}.pt'))
-    model.eval()
-
-    model.eval()  # Set the model to evaluation mode
-    input_data = Variable(input_data, requires_grad=True)  # Wrap input data in a Variable with gradient tracking
-    # output = model(input_data)  # Forward pass
-    if args.contrastive_mid_layer:
-        output, _ = model((input_data.permute(0, 2, 1)).unsqueeze(2))
-    else:
-        output = model((input_data.permute(0, 2, 1)).unsqueeze(2))
-
-    # Initialize a list to store saliency maps for each output
-    saliency_maps = []
-
-    # Compute and store the gradients of the input data for each output
-    for i in range(4):
-        model.zero_grad()  # Reset gradients
-        output[0, i].backward(retain_graph=True)  # Backward pass to compute gradients
-
-        # Get the gradients of the input data
-        saliency_map = input_data.grad.abs()[0]
-        saliency_map = saliency_map.squeeze().detach().cpu().numpy()  # Remove singleton dimensions and convert to NumPy array
-
-        # Append saliency map to the list
-        saliency_maps.append(saliency_map)
-
-    # Plot all four saliency maps in the same plot
-    plt.figure(figsize=(12, 6))  # Increase figure size for better readability
-    for i, saliency_map in enumerate(saliency_maps):
-        ax = plt.subplot(2, 2, i + 1)
-        sns.heatmap(saliency_map, cmap='hot', ax=ax)  # Plot heatmap
-        plt.title(f'Output {i + 1} Saliency Map')
-        plt.yticks(ticks=range(saliency_map.shape[0]), labels=range(1, saliency_map.shape[0] + 1))
-
-    plt.tight_layout()  # Adjust subplot layout to prevent overlap
-    plt.show()
-    return model
 if __name__ == '__main__':
     args = configArgsPCT()
     model = train_and_test(args)
