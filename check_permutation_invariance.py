fixed stupid bug of wrong graph weighting

check_permutation_invariance.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 import numpy as np
 from scipy.spatial.distance import cdist
 from scipy.linalg import eigh
-
+from plotting_functions import *
 
 def createLap(point_cloud, normalized, graph_weight_mode):
     distances = cdist(point_cloud, point_cloud)
@@ -92,6 +92,7 @@ def check(graph_weight_mode=0):
     num_point_clouds = len(point_clouds_group)
     normalize_lap = [True, False]
     noise_size = [0, 0.001, 0.01, 0.1, 1]
+    # noise_size = [0]
     all_runs_same_index_list = {}
     for nlap in normalize_lap:
         for std in noise_size:
@@ -116,18 +117,21 @@ def check(graph_weight_mode=0):
 
                 pcl_size = len(point_cloud)
                 rot = R.random().as_matrix()
+                # print(rot)
                 point_cloud1 = np.matmul(point_cloud, rot.T)
+                # point_cloud1 = point_cloud
                 noise = np.random.normal(0, std, point_cloud.shape)
                 # noise = np.random.normal(0, 0, point_cloud.shape)
                 noisy_point_cloud = point_cloud1 + noise
                 permuted_indices = np.concatenate(([0], (1 + np.random.permutation(pcl_size-1))))
+                # permuted_indices = np.arange(41)
                 noisy_point_cloud = noisy_point_cloud[permuted_indices]
 
 
-                l = createLap(noisy_point_cloud, nlap, graph_weight_mode=0)
+                l2 = createLap(noisy_point_cloud, nlap, graph_weight_mode=graph_weight_mode)
                 # Compute LPE embedding
-                eigvecs, eigenvals = top_k_smallest_eigenvectors(l)
-                indices_noised, fixed_eigs = sort_by_first_eigenvector(eigvecs)
+                eigvecs2, eigenvals2 = top_k_smallest_eigenvectors(l2)
+                indices_noised, fixed_eigs2 = sort_by_first_eigenvector(eigvecs2)
 
                 base = np.arange(pcl_size)
 
@@ -139,7 +143,7 @@ def check(graph_weight_mode=0):
 
             all_runs_same_index_list[run_name] = same_index_list
             print(f'mean: {np.mean(same_index_list)}')
-    plot_same_index_list(all_runs_same_index_list)
+    # plot_same_index_list(all_runs_same_index_list)
 
 if __name__ == '__main__':
     check(graph_weight_mode=0)
@@ -147,8 +151,6 @@ if __name__ == '__main__':
     check(graph_weight_mode=2)
     check(graph_weight_mode=3)
     check(graph_weight_mode=4)
-    check(graph_weight_mode=5)
-    check(graph_weight_mode=3)
     check(graph_weight_mode=6)
     check(graph_weight_mode=7)
 